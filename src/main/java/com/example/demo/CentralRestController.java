efd0c81f6e742d011c371b3e7d3dea11994b4ff9
eimamoglu <eimamoglu@student.tgm.ac.at>
2025-11-30 17:28:25 +0100

    CentralRestController für Json/Xml Getmapping

---
+++ src/main/java/com/example/demo/CentralRestController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/central")
+public class CentralRestController {
+
+    @Autowired
+    private CentralDataAggregator aggregator;
+
+    @GetMapping(value = "/stock", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<WarehouseData> getJsonStock() {
+        return aggregator.getAll();
+    }
+
+    @GetMapping(value = "/stock.xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public WarehousesXml getXmlStock() {
+        return new WarehousesXml(aggregator.getAll());
+    }
+}
